920641ec29b507a4e2a3ce7f1f22157dd256d0f9
Sanjaymg7 <sanjay.m@juegostudioz.com>
2022-08-11 09:44:00 +0530

    FEAT:Updated file structure

---
+++ Employee-Leave-Management/Model/Employee.swift
@@ -0,0 +1,18 @@
+//
+//  Employee.swift
+//  Employee-Leave-Management
+//
+//  Created by Sanjay M G on 11/08/22.
+//
+
+import UIKit
+
+struct Employee:Codable {
+    var employeeId:String
+    var fullName:String
+    var email:String
+    var isManager:Bool
+    var password:String
+    var profilePicture:Data
+    var managerID:String
+}
